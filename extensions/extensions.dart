b62c9ae331ebd3fd0e2278f39fbf7c0fb5166152
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2025-01-05 22:09:15 +0200

    Update \Extension

---
+++ extensions/extensions.dart
@@ -0,0 +1,11 @@
+export 'package:my_banking_guide/core/utils/extensions/bloc_extensions.dart';
+export 'package:my_banking_guide/core/utils/extensions/gesture_extensions.dart';
+export 'package:my_banking_guide/core/utils/extensions/navigation_extensions.dart';
+export 'package:my_banking_guide/core/utils/extensions/padding_extensions.dart';
+export 'package:my_banking_guide/core/utils/extensions/theme_extensions.dart';
+export 'package:my_banking_guide/core/utils/extensions/alignment_extensions.dart';
+export 'package:my_banking_guide/core/utils/extensions/margin_extensions.dart';
+export 'package:my_banking_guide/core/utils/extensions/media_query_extensions.dart';
+export 'package:my_banking_guide/core/utils/extensions/sizedbox_extensions.dart';
+export 'package:my_banking_guide/core/utils/extensions/transform_extensions.dart';
+export 'package:my_banking_guide/core/utils/extensions/visibility_extensions.dart';
